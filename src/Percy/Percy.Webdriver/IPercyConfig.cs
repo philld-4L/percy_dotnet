b62e8da88d2aad3ae3e4badf37cb93f23393d754
philldorrell <phill.dorrell@googlemail.com>
2021-05-07 22:48:00 +0100

    Webdriver methods and config interface
    Posting of snapshots will be handled by a seperate class
    
    IPercyConfig will have multiple implementations to allow it to be loaded via xml and json depending on the solution implementing it

---
+++ src/Percy/Percy.Webdriver/IPercyConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Percy.Webdriver
+{
+    public interface IPercyConfig
+    {
+        /// <summary>
+        /// Percy Server address
+        /// </summary>
+        string PercyServer { get; set; }
+        string PercyDOM();
+        string ClientInfo { get; }
+    }
+}
---
+++ src/Percy/Percy.Webdriver/WebdriverExtensions.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Percy.Webdriver
+{
+    /// <summary>
+    /// Extension methods for IWebdriver
+    /// </summary>
+    public static class WebdriverExtensions
+    {
+        /// <summary>
+        /// Capture a snapshot for percy
+        /// </summary>
+        /// <param name="driver">Webdriver context we are capturing the snapshot from</param>
+        public static string BuildSnapshot(this IWebDriver driver, IPercyConfig config, bool enableJavascript = false)
+        {
+            var jsExe = (IJavaScriptExecutor)driver;
+            //fetch the DOM
+            jsExe.ExecuteScript(config.PercyDOM());
+            string snapshot = "return PercyDOM.serialize({ enableJavascript: "+ enableJavascript.ToString() + "})\n";
+            //now create the snapshot
+            return jsExe.ExecuteScript(snapshot).ToString();
+
+        }
+
+        /// <summary>
+        /// Fetches the environment info for the WebDriver instance being used
+        /// </summary>
+        /// <param name="driver">Our webdriver instance</param>
+        /// <returns>The instance's type</returns>
+        public static string EnvironmentInfo(this IWebDriver driver)
+        {
+            return driver.GetType().FullName;
+        }
+
+    
+    }
+}
